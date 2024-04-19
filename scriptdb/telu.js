ff83878eae7d165c08996eeb416c6b10a9af4a16
r12a <ishida@w3.org>
2024-04-19 08:30:20 +0100

    te: Feb template update.

--- scriptdb/telu.js
+++ scriptdb/telu.js
@@ -6,8 +6,8 @@ var spreadsheet = `telu-te	key	ipa	ipa+	translit	transc	kbd	class	status	type	us
 ,	,			,	,		Po		comma							U+002C: COMMA	
 ;	;			;	;		Po		semicolon							U+003B: SEMICOLON	
 :	:			:	:		Po		colon							U+003A: COLON	
-ౕ	:			ː			Mn	u	length mark	not used						U+0C55: TELUGU LENGTH MARK	✓
-ౖ	:			ˉ			Mn	r	length mark	use in NFD only						U+0C56: TELUGU AI LENGTH MARK	✓
+ౕ	:			ː			Mn	u	length mark							U+0C55: TELUGU LENGTH MARK	✓
+ౖ	:			ˉ			Mn	r	length mark	Only found in decomposed text.						U+0C56: TELUGU AI LENGTH MARK	✓
 !	!			!	!		Po		exclamation mark							U+0021: EXCLAMATION MARK	
 ?	?			?	?		Po		question mark							U+003F: QUESTION MARK	
 .	.			.	.		Po		full stop							U+002E: FULL STOP	
@@ -56,66 +56,81 @@ var spreadsheet = `telu-te	key	ipa	ipa+	translit	transc	kbd	class	status	type	us
 ౾	$			2⁶			No	a	fraction							U+0C7E: TELUGU FRACTION DIGIT THREE FOR EVEN POWERS OF FOUR	✓
 ౿	$			3⁶			So	a	fraction							U+0C7F: TELUGU SIGN TUUMU	✓
 																	
-ా	a	aː æː		ā	a		Mn		vowel sign			ఆ				U+0C3E: TELUGU VOWEL SIGN AA	✓
-ై	a	aj		aʲ	ai		Mn		diphthong vowel sign			ఐ				U+0C48: TELUGU VOWEL SIGN AI	✓
-ౌ	a	aw		aʷ	au		Mn		diphthong vowel sign			ఔ				U+0C4C: TELUGU VOWEL SIGN AU	✓
-అ	A	a		ạ	a		Lo		independent vowel							U+0C05: TELUGU LETTER A	✓
-ఆ	A	aː		ạ̄	ā		Lo		independent vowel		ా					U+0C06: TELUGU LETTER AA	✓
-ఐ	A	aj		ạʲ	ai		Lo		independent diphthong		ై					U+0C10: TELUGU LETTER AI	✓
-ఔ	A	aw		ạʷ	aw		Lo		independent diphthong		ౌ					U+0C14: TELUGU LETTER AU	✓
+ప	p¶	p	a	p	p		Lo		consonant				్ప			U+0C2A: TELUGU LETTER PA	✓
+ఫ	P	pʰ	a	pʰ	ph		Lo		consonant				్ఫ			U+0C2B: TELUGU LETTER PHA	✓
 బ	b	b	a	b	b		Lo		consonant				్బ			U+0C2C: TELUGU LETTER BA	✓
 భ	B	bʰ	a	bʰ	bh		Lo		consonant				్భ			U+0C2D: TELUGU LETTER BHA	✓
+త	t	t	a	t	t		Lo		consonant				్త			U+0C24: TELUGU LETTER TA	✓
+థ	T	tʰ	a	tʰ	th		Lo		consonant				్థ			U+0C25: TELUGU LETTER THA	✓
 చ	c	t͡ʃ t͡s	a	c	c		Lo		consonant				్చ			U+0C1A: TELUGU LETTER CA	✓
 ఛ	C	t͡ʃʰ	a	cʰ	ch		Lo		consonant				్ఛ			U+0C1B: TELUGU LETTER CHA	✓
 ద	d	d	a	d	d		Lo		consonant				్ద			U+0C26: TELUGU LETTER DA	✓
 ధ	D	dʰ	a	dʰ	dh		Lo		consonant				్ధ			U+0C27: TELUGU LETTER DHA	✓
-డ	d	ɖ	a	ɖ	ḍ		Lo		consonant				్డ			U+0C21: TELUGU LETTER DDA	✓
-ఢ	D	ɖʰ	a	ɖʰ	ḍh		Lo		consonant				్ఢ			U+0C22: TELUGU LETTER DDHA	✓
-ె	e	e		e	e		Mn		vowel sign			ఎ				U+0C46: TELUGU VOWEL SIGN E	✓
-ే	e	eː æː		ē	ē		Mn		vowel sign			ఏ				U+0C47: TELUGU VOWEL SIGN EE	✓
-ఎ	E	eː		ẹ	e		Lo		independent vowel		ె					U+0C0E: TELUGU LETTER E	✓
-ఏ	E	eː		ẹ̄	ē		Lo		independent vowel		ే					U+0C0F: TELUGU LETTER EE	✓
-గ	g	ɡ	a	g	g		Lo		consonant				్గ			U+0C17: TELUGU LETTER GA	✓
-ఘ	G	ɡʰ	a	gʰ	gh		Lo		consonant				్ఘ			U+0C18: TELUGU LETTER GHA	✓
-హ	h	h	a	h	h		Lo		consonant				్హ			U+0C39: TELUGU LETTER HA	✓
-ః	h	ha		h̽	ḥ		Mc		visarga							U+0C03: TELUGU SIGN VISARGA	✓
-ి	i	i		i	ē		Mn		vowel sign			ఇ				U+0C3F: TELUGU VOWEL SIGN I	✓
-ీ	i	iː		ī	ī		Mn		vowel sign			ఈ				U+0C40: TELUGU VOWEL SIGN II	✓
-ఇ	I	i		ị	i		Lo		independent vowel		ి					U+0C07: TELUGU LETTER I	✓
-ఈ	I	iː		ị̄	ī		Lo		independent vowel		ీ					U+0C08: TELUGU LETTER II	✓
 జ	j	d͡ʒ d͡z	a	ɟ	j		Lo		consonant				్జ			U+0C1C: TELUGU LETTER JA	✓
 ఝ	J	d͡ʒʰ	a	ɟʰ	jh		Lo		consonant				్ఝ			U+0C1D: TELUGU LETTER JHA	✓
+ట	t	ʈ	a	ʈ	ṭ		Lo		consonant				్ట			U+0C1F: TELUGU LETTER TTA	✓
+ఠ	T	ʈʰ	a	ʈʰ	ṭh		Lo		consonant				్ఠ			U+0C20: TELUGU LETTER TTHA	✓
+డ	d	ɖ	a	ɖ	ḍ		Lo		consonant				్డ			U+0C21: TELUGU LETTER DDA	✓
+ఢ	D	ɖʰ	a	ɖʰ	ḍh		Lo		consonant				్ఢ			U+0C22: TELUGU LETTER DDHA	✓
 క	k	k	a	k	k		Lo		consonant				్క			U+0C15: TELUGU LETTER KA	✓
 ఖ	K	kʰ	a	kʰ	kh		Lo		consonant				్ఖ			U+0C16: TELUGU LETTER KHA	✓
-ల	l	l	a	l	l		Lo		consonant				్ల			U+0C32: TELUGU LETTER LA	✓
-ళ	l	ɭ	a	ɭ	ḷ		Lo		consonant				్ళ			U+0C33: TELUGU LETTER LLA	✓
-మ	m	m	a	m	m		Lo		consonant				్మ			U+0C2E: TELUGU LETTER MA	✓
-ం	m	m n ŋ		m̽	ṁ		Mc		anusvara							U+0C02: TELUGU SIGN ANUSVARA	✓
-న	n	n	a	n	n		Lo		consonant				్న			U+0C28: TELUGU LETTER NA	✓
-ఞ	n	ɲ	a	ɲ	ñ		Lo		consonant				్ఞ			U+0C1E: TELUGU LETTER NYA	✓
-ణ	n	ɳ	a	ɳ	ṇ		Lo		consonant				్ణ			U+0C23: TELUGU LETTER NNA	✓
-ఙ	n	ɳ	a	ŋ	ṇ		Lo		consonant				్ఙ			U+0C19: TELUGU LETTER NGA	✓
-ొ	o	o		o	o		Mn		vowel sign			ఒ				U+0C4A: TELUGU VOWEL SIGN O	✓
-ో	o	oː		ō	ō		Mn		vowel sign			ఓ				U+0C4B: TELUGU VOWEL SIGN OO	✓
-ఒ	O	o		ọ	o		Lo		independent vowel		ొ					U+0C12: TELUGU LETTER O	✓
-ఓ	O	oː		ọ̄	ō		Lo		independent vowel		ో					U+0C13: TELUGU LETTER OO	✓
-ఫ	P	pʰ	a	pʰ	ph		Lo		consonant				్ఫ			U+0C2B: TELUGU LETTER PHA	✓
-ప	p¶	p	a	p	p		Lo		consonant				్ప			U+0C2A: TELUGU LETTER PA	✓
-ర	r	r	a	r	r		Lo		consonant				్ర			U+0C30: TELUGU LETTER RA	✓
-ఱ	r	ɽ	a	ɽ	ṛ		Lo		consonant				్ఱ			U+0C31: TELUGU LETTER RRA	✓
+గ	g	ɡ	a	g	g		Lo		consonant				్గ			U+0C17: TELUGU LETTER GA	✓
+ఘ	G	ɡʰ	a	gʰ	gh		Lo		consonant				్ఘ			U+0C18: TELUGU LETTER GHA	✓
 స	s	s	a	s	s		Lo		consonant				్స			U+0C38: TELUGU LETTER SA	✓
 శ	s	ʃ	a	ʃ	ś		Lo		consonant				్శ			U+0C36: TELUGU LETTER SHA	✓
 ష	s	ʂ	a	ʂ	ṣ		Lo		consonant				్ష			U+0C37: TELUGU LETTER SSA	✓
-త	t	t	a	t	t		Lo		consonant				్త			U+0C24: TELUGU LETTER TA	✓
-థ	T	tʰ	a	tʰ	th		Lo		consonant				్థ			U+0C25: TELUGU LETTER THA	✓
-ట	t	ʈ	a	ʈ	ṭ		Lo		consonant				్ట			U+0C1F: TELUGU LETTER TTA	✓
-ఠ	T	ʈʰ	a	ʈʰ	ṭh		Lo		consonant				్ఠ			U+0C20: TELUGU LETTER TTHA	✓
-ు	u	u		u	u		Mc		vowel sign			ఉ				U+0C41: TELUGU VOWEL SIGN U	✓
-ూ	u	uː		ū	ū		Mc		vowel sign			ఊ				U+0C42: TELUGU VOWEL SIGN UU	✓
-ఉ	U	u		ụ	u		Lo		independent vowel		ు					U+0C09: TELUGU LETTER U	✓
-ఊ	U	uː		ụ̄	ū		Lo		independent vowel		ూ					U+0C0A: TELUGU LETTER UU	✓
+హ	h	h	a	h	h		Lo		consonant				్హ			U+0C39: TELUGU LETTER HA	✓
+మ	m	m	a	m	m		Lo		consonant				్మ			U+0C2E: TELUGU LETTER MA	✓
+న	n	n	a	n	n		Lo		consonant				్న			U+0C28: TELUGU LETTER NA	✓
+ఞ	n	ɲ	a	ɲ	ñ		Lo		consonant				్ఞ			U+0C1E: TELUGU LETTER NYA	✓
+ణ	n	ɳ	a	ɳ	ṇ		Lo		consonant				్ణ			U+0C23: TELUGU LETTER NNA	✓
+ఙ	n	ŋ	a	ŋ	ṇ		Lo	r	consonant				్ఙ			U+0C19: TELUGU LETTER NGA	✓
 వ	v	ʋ	a	ʋ	v		Lo		consonant				్వ			U+0C35: TELUGU LETTER VA	✓
+ర	r	r	a	r	r		Lo		consonant				్ర			U+0C30: TELUGU LETTER RA	✓
+ఱ	r	ɽ	a	ɽ	ṛ		Lo		consonant				్ఱ			U+0C31: TELUGU LETTER RRA	✓
+ల	l	l	a	l	l		Lo		consonant				్ల			U+0C32: TELUGU LETTER LA	✓
+ళ	l	ɭ	a	ɭ	ḷ		Lo		consonant				్ళ			U+0C33: TELUGU LETTER LLA	✓
 య	y	j	a	y	y		Lo		consonant				్య			U+0C2F: TELUGU LETTER YA	✓
+																	
+ః	h	ha		h̽	ḥ		Mc		final consonant						visarga	U+0C03: TELUGU SIGN VISARGA	✓
+ం	m	m n ŋ		m̽	ṁ		Mc		final consonant						anusvara	U+0C02: TELUGU SIGN ANUSVARA	✓
+																	
+																	
+ి	i	i		i	ē		Mn		vowel sign			ఇ				U+0C3F: TELUGU VOWEL SIGN I	✓
+ఇ	I	i		ị	i		Lo		independent vowel		ి					U+0C07: TELUGU LETTER I	✓
+ీ	i	iː		ī	ī		Mn		vowel sign			ఈ				U+0C40: TELUGU VOWEL SIGN II	✓
+ఈ	I	iː		ị̄	ī		Lo		independent vowel		ీ					U+0C08: TELUGU LETTER II	✓
+ు	u	u		u	u		Mc		vowel sign			ఉ				U+0C41: TELUGU VOWEL SIGN U	✓
+ఉ	U	u		ụ	u		Lo		independent vowel		ు					U+0C09: TELUGU LETTER U	✓
+ూ	u	uː		ū	ū		Mc		vowel sign			ఊ				U+0C42: TELUGU VOWEL SIGN UU	✓
+ఊ	U	uː		ụ̄	ū		Lo		independent vowel		ూ					U+0C0A: TELUGU LETTER UU	✓
+ె	e	e		e	e		Mn		vowel sign			ఎ				U+0C46: TELUGU VOWEL SIGN E	✓
+ఎ	E	e		ẹ	e		Lo		independent vowel		ె					U+0C0E: TELUGU LETTER E	✓
+ే	e	eː æː		ē	ē		Mn		vowel sign			ఏ				U+0C47: TELUGU VOWEL SIGN EE	✓
+ఏ	E	eː		ẹ̄	ē		Lo		independent vowel		ే					U+0C0F: TELUGU LETTER EE	✓
+ొ	o	o		o	o		Mn		vowel sign			ఒ				U+0C4A: TELUGU VOWEL SIGN O	✓
+ఒ	O	o		ọ	o		Lo		independent vowel		ొ					U+0C12: TELUGU LETTER O	✓
+ో	o	oː		ō	ō		Mn		vowel sign			ఓ				U+0C4B: TELUGU VOWEL SIGN OO	✓
+ఓ	O	oː		ọ̄	ō		Lo		independent vowel		ో					U+0C13: TELUGU LETTER OO	✓
+అ	A	a		ạ	a		Lo		independent vowel							U+0C05: TELUGU LETTER A	✓
+ా	a	aː æː		ā	a		Mn		vowel sign			ఆ				U+0C3E: TELUGU VOWEL SIGN AA	✓
+ఆ	A	aː		ạ̄	ā		Lo		independent vowel		ా					U+0C06: TELUGU LETTER AA	✓
+																	
+ై	a	aj		aʲ	ai		Mn		diphthong vowel sign			ఐ				U+0C48: TELUGU VOWEL SIGN AI	✓
+\u{C46}\u{C56}		aj						r	diphthong vowel sign	Only found in decomposed text.						U+0C46 U+0C56: TELUGU VOWEL SIGN E, AI LENGTH MARK	
+ఐ	A	aj		ạʲ	ai		Lo		independent diphthong		ై					U+0C10: TELUGU LETTER AI	✓
+ౌ	a	aw		aʷ	au		Mn		diphthong vowel sign			ఔ				U+0C4C: TELUGU VOWEL SIGN AU	✓
+ఔ	A	aw		ạʷ	aw		Lo		independent diphthong		ౌ					U+0C14: TELUGU LETTER AU	✓
+																	
+ృ		ru		r̥	r̥		Mc		vocalic vowel sign			ఋ				U+0C43: TELUGU VOWEL SIGN VOCALIC R	✓
+ౢ		ruː		r̥̄	r̥̄		Mn	u	vocalic vowel sign	For Sanskrit.		ౠ				U+0C62: TELUGU VOWEL SIGN VOCALIC L	✓
+ౄ		alu		l̥	l̥		Mc	u	vocalic vowel sign	For Sanskrit.		ఌ				U+0C44: TELUGU VOWEL SIGN VOCALIC RR	✓
+ౣ		alu		l̥̄	l̥̄		Mn	u	vocalic vowel sign	For Sanskrit.		ౡ				U+0C63: TELUGU VOWEL SIGN VOCALIC LL	✓
+																	
+ఋ		ru		r̥̣	r̥		Lo		independent vocalic		ృ					U+0C0B: TELUGU LETTER VOCALIC R	✓
+ౠ		ruː		r̥̣̄	r̥̄		Lo	u	independent vocalic	For Sanskrit.	ౢ					U+0C60: TELUGU LETTER VOCALIC RR	✓
+ఌ		alu		l̥̣	l̥		Lo	u	independent vocalic	For Sanskrit.	ౄ					U+0C0C: TELUGU LETTER VOCALIC L	✓
+ౡ		alu:		l̥̣̄	l̥̄		Lo	u	independent vocalic	For Sanskrit.	ౣ					U+0C61: TELUGU LETTER VOCALIC LL	✓
 																	
 																	
 ఽ							Lo	a	avagraha				్ఽ			U+0C3D: TELUGU SIGN AVAGRAHA	✓
@@ -124,21 +139,7 @@ var spreadsheet = `telu-te	key	ipa	ipa+	translit	transc	kbd	class	status	type	us
 ౚ		?	a				Lo	a	consonant				్ౚ			U+0C5A: TELUGU LETTER RRRA	✓
 ఴ		ʐ	a	ʐ			Lo	a	consonant				్ఴ			U+0C34 TELUGU LETTER LLLA	✓
 																	
-																	
-\u{C46}\u{C56}		aj						r	decomposed ai	NFD only						U+0C46 U+0C56: TELUGU VOWEL SIGN E, AI LENGTH MARK	
-																	
-ృ		ru		r̥	r̥		Mc		vocalic vowel sign			ఋ				U+0C43: TELUGU VOWEL SIGN VOCALIC R	✓
-ౢ		ruː		r̥̄	r̥̄		Mn	u	vocalic vowel sign	for Sanskrit		ౠ				U+0C62: TELUGU VOWEL SIGN VOCALIC L	✓
-ౄ		alu		l̥	l̥		Mc	u	vocalic vowel sign	for Sanskrit		ఌ				U+0C44: TELUGU VOWEL SIGN VOCALIC RR	✓
-ౣ		alu		l̥̄	l̥̄		Mn	u	vocalic vowel sign	for Sanskrit		ౡ				U+0C63: TELUGU VOWEL SIGN VOCALIC LL	✓
-																	
-ఋ		ru		r̥̣	r̥		Lo		independent vocalic		ృ					U+0C0B: TELUGU LETTER VOCALIC R	✓
-ౠ		ruː		r̥̣̄	r̥̄		Lo	u	independent vocalic	for Sanskrit	ౢ					U+0C60: TELUGU LETTER VOCALIC RR	✓
-ఌ		alu		l̥̣	l̥		Lo	u	independent vocalic	for Sanskrit	ౄ					U+0C0C: TELUGU LETTER VOCALIC L	✓
-ౡ		alu:		l̥̣̄	l̥̄		Lo	u	independent vocalic	for Sanskrit	ౣ					U+0C61: TELUGU LETTER VOCALIC LL	✓
-																	
 ౷							Po	?								U+0C77: TELUGU SIGN SIDDHAM	✓
-																	
 ఄ							Mn	?								U+0C04: TELUGU SIGN COMBINING ANUSVARA ABOVE	✓
 																	
 ʼ				ʼ	ʼ		Lm	?	apostrophe							U+02BC: MODIFIER LETTER APOSTROPHE	

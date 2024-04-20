d03dfbe83a3627b9f24832f84e40ca5a64f56c77
r12a <ishida@w3.org>
2024-04-20 05:09:38 +0100

    ka: Feb template update.

--- scriptdb/geor-ka.js
+++ scriptdb/geor-ka.js
@@ -17,82 +17,85 @@ var spreadsheet = `geor	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage
 „	<			“	“		Pi		quotation mark							U+201E: DOUBLE LOW-9 QUOTATION MARK				
 »	>			»	”		Pf		quotation mark							U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK				
 “	>			›			Pf		quotation mark							U+201C: LEFT DOUBLE QUOTATION MARK				
-ა	a	ɑ		a	a	g	Ll		vowel	mkhedruli		Ა	1		ani	U+10D0: GEORGIAN LETTER AN	a	a	a	✓
-Ა	A	ɑ		A	a	G	Lu		vowel	mtavruli	ა		1		ani	U+1C90: GEORGIAN MTAVRULI CAPITAL LETTER AN	a	a	a	✓
-ბ	b	b		b	b	.	Ll		consonant	mkhedruli		Ბ	2		bani	U+10D1: GEORGIAN LETTER BAN	b	b	b	✓
-Ბ	B	b		B	b	>	Lu		consonant	mtavruli	ბ		2		bani	U+1C91: GEORGIAN MTAVRULI CAPITAL LETTER BAN	b	b	b	✓
-ჩ	c	t͡ʃʰ		ʧ	ch	x	Ll		consonant	mkhedruli		Ჩ	1000		chini	U+10E9: GEORGIAN LETTER CHIN	č'	ch'	ç	✓
-ჭ	c	t͡ʃʼ		ʧ̇	ch'	z	Ll		consonant	mkhedruli		Ჭ	5000		ch'ari	U+10ED: GEORGIAN LETTER CHAR	č	ch	ç̌	✓
-Ჩ	C	t͡ʃʰ		Tᶴ	ch	X	Lu		consonant	mtavruli	ჩ		1000		chini	U+1CA9: GEORGIAN MTAVRULI CAPITAL LETTER CHIN	č'	ch'	ç	✓
-Ჭ	C	t͡ʃʼ		Ṫᶴ	ch'	Z	Lu		consonant	mtavruli	ჭ		5000		ch'ari	U+1CAD: GEORGIAN MTAVRULI CAPITAL LETTER CHAR	č	ch	ç̌	✓
-დ	d	d		d	d	;	Ll		consonant	mkhedruli		Დ	4		doni	U+10D3: GEORGIAN LETTER DON	d	d	d	✓
-ძ	d	d͡z		ʣ	dz	s	Ll		consonant	mkhedruli		Ძ	3000		dzili	U+10EB: GEORGIAN LETTER JIL	j	dz	ž	✓
-Დ	D	d		D	d	:	Lu		consonant	mtavruli	დ		4		doni	U+1C93: GEORGIAN MTAVRULI CAPITAL LETTER DON	d	d	d	✓
-Ძ	D	d͡z		ǲ	dz	S	Lu		consonant	mtavruli	ძ		3000		dzili	U+1CAB: GEORGIAN MTAVRULI CAPITAL LETTER JIL	j	dz	ž	✓
-ე	e	ɛ		ɛ	e	t	Ll		vowel	mkhedruli		Ე	5		eni	U+10D4: GEORGIAN LETTER EN	e	e	e	✓
-ჱ	e	eɪ		e		y	Ll	r	vowel	mkhedruli		Ჱ	8		he	U+10F1: GEORGIAN LETTER HE	ē	ey		✓
-Ე	E	ɛ		Ɛ	e	T	Lu		vowel	mtavruli	ე		5		eni	U+1C94: GEORGIAN MTAVRULI CAPITAL LETTER EN	e	e	e	✓
-Ჱ	E	eɪ		E		T	Lu	o	vowel	mtavruli	ჱ		8		he	U+1CB1: GEORGIAN MTAVRULI CAPITAL LETTER HE	ē	ey		✓
-გ	g	ɡ		g	g	u	Ll		consonant	mkhedruli		Გ	3		gani	U+10D2: GEORGIAN LETTER GAN	g	g	g	✓
-Გ	G	ɡ		G	g	U	Lu		consonant	mtavruli	გ		3		gani	U+1C92: GEORGIAN MTAVRULI CAPITAL LETTER GAN	g	g	g	✓
-ჰ	h	h		h	h	?	Ll		consonant	mkhedruli		Ჰ	9000		hae	U+10F0: GEORGIAN LETTER HAE	h	h	h	✓
-Ჰ	H	h		H	h	?	Lu		consonant	mtavruli	ჰ		9000		hae	U+1CB0: GEORGIAN MTAVRULI CAPITAL LETTER HAE	h	h	h	✓
-ი	i	i		i	i	n	Ll		vowel	mkhedruli		Ი	10		ini	U+10D8: GEORGIAN LETTER IN	i	i	i	✓
-Ი	I	i		I	i	N	Lu		vowel	mtavruli	ი		10		ini	U+1C98: GEORGIAN MTAVRULI CAPITAL LETTER IN	i	i	i	✓
-ჯ	j	d͡ʒ		ʤ	j	w	Ll		consonant	mkhedruli		Ჯ	8000		jani	U+10EF: GEORGIAN LETTER JHAN	ǰ	j	c	✓
-ჲ	j	je		j		n	Ll	r	consonant	mkhedruli		Ჲ	60		hie	U+10F2: GEORGIAN LETTER HIE	y	j	y	✓
-Ჯ	J	d͡ʒ		Dᶾ	j	W	Lu		consonant	mtavruli	ჯ		8000		jani	U+1CAF: GEORGIAN MTAVRULI CAPITAL LETTER JHAN	ǰ	j	c	✓
-Ჲ	J	je		J		N	Lu	o	mtavruli letter	mtavruli	ჲ		60		hie	U+1CB2: GEORGIAN MTAVRULI CAPITAL LETTER HIE	y	j	y	✓
-ქ	k	kʰ		k	k	,	Ll		consonant	mkhedruli		Ქ	600		kani	U+10E5: GEORGIAN LETTER KHAR	k'	k'	k	✓
-კ	k	kʼ		k̇	k'	r	Ll		consonant	mkhedruli		Კ	20		k'ani	U+10D9: GEORGIAN LETTER KAN	k	k	ǩ	✓
-Ქ	K	kʰ		K	k	<	Lu		consonant	mtavruli	ქ		600		kani	U+1CA5: GEORGIAN MTAVRULI CAPITAL LETTER KHAR	k'	k'	k	✓
-Კ	K	kʼ		K̇	k'		Lu		consonant	mtavruli	კ		20		k'ani	U+1C99: GEORGIAN MTAVRULI CAPITAL LETTER KAN	k	k	ǩ	✓
-ლ	l	l		l	l	l	Ll		consonant	mkhedruli		Ლ	30		lasi	U+10DA: GEORGIAN LETTER LAS	l	l	l	✓
-Ლ	L	l		L	l	L	Lu		consonant	mtavruli	ლ		30		lasi	U+1C9A: GEORGIAN MTAVRULI CAPITAL LETTER LAS	l	l	l	✓
-მ	m	m		m	m	b	Ll		consonant	mkhedruli		Მ	40		mani	U+10DB: GEORGIAN LETTER MAN	m	m	m	✓
-Მ	M	m		M	m	B	Lu		consonant	mtavruli	მ		40		mani	U+1C9B: GEORGIAN MTAVRULI CAPITAL LETTER MAN	m	m	m	✓
-ნ	n	n		n	n	y	Ll		consonant	mkhedruli		Ნ	50		nari	U+10DC: GEORGIAN LETTER NAR	n	n	n	✓
-Ნ	N	n		N	n	Y	Lu		consonant	mtavruli	ნ		50		nari	U+1C9C: GEORGIAN MTAVRULI CAPITAL LETTER NAR	n	n	n	✓
-ო	o	ɔ		ɔ	o	k	Ll		vowel	mkhedruli		Ო	70		oni	U+10DD: GEORGIAN LETTER ON	o	o	o	✓
-ჵ	o	oː		o		/	Ll	a	vowel	mkhedruli		Ჵ	10000		hoe	U+10F5: GEORGIAN LETTER HOE	ō			✓
-Ო	O	ɔ		Ɔ	o	K	Lu		vowel	mtavruli	ო		70		oni	U+1C9D: GEORGIAN MTAVRULI CAPITAL LETTER ON	o	o	o	✓
-Ჵ	O	oː		O		?	Lu	a	vowel	mtavruli	ჵ		10000		hoe	U+1CB5: GEORGIAN MTAVRULI CAPITAL LETTER HOE	ō			✓
-ფ	p¶	pʰ		p	p	a	Ll		consonant	mkhedruli		Ფ	500		pari	U+10E4: GEORGIAN LETTER PHAR	p'	p'	p	✓
-პ	p	pʼ		ṗ	p'	h	Ll		consonant	mkhedruli		Პ	80		p'ari	U+10DE: GEORGIAN LETTER PAR	p	p	ṗ	✓
-Ფ	P	pʰ		P	p	A	Lu		consonant	mtavruli	ფ		500		pari	U+1CA4: GEORGIAN MTAVRULI CAPITAL LETTER PHAR	p'	p'	p	✓
-Პ	P	pʼ		Ṗ	p'	H	Lu		consonant	mtavruli	პ		80		p'ari	U+1C9E: GEORGIAN MTAVRULI CAPITAL LETTER PAR	p	p	ṗ	✓
-ყ	q	qʼ		q̇	q'	c	Ll		consonant	mkhedruli		Ყ	800		q'ari	U+10E7: GEORGIAN LETTER QAR	q	q	q	✓
-ჴ	q	qʰ		q		[	Ll	r	consonant	mkhedruli		Ჴ	7000		qari, hari	U+10F4: GEORGIAN LETTER HAR	ẖ	q'		✓
-Ყ	Q	qʼ		Q̇	q'	C	Lu		consonant	mtavruli	ყ		800		q'ari	U+1CA7: GEORGIAN MTAVRULI CAPITAL LETTER QAR	q	q	q	✓
-Ჴ	Q	qʰ		Q		{	Lu	o	consonant	mtavruli	ჴ		7000		qari, hari	U+1CB4: GEORGIAN MTAVRULI CAPITAL LETTER HAR	ẖ	q'		✓
-რ	r	r		r	r	j	Ll		consonant	mkhedruli		Რ	100		rae	U+10E0: GEORGIAN LETTER RAE	r	r	r	✓
-Რ	R	r		R	r	J	Lu		consonant	mtavruli	რ		100		rae	U+1CA0: GEORGIAN MTAVRULI CAPITAL LETTER RAE	r	r	r	✓
-ს	s	s		s	s	v	Ll		consonant	mkhedruli		Ს	200		sani	U+10E1: GEORGIAN LETTER SAN	s	s	s	✓
-შ	s	ʃ		ʃ	sh	i	Ll		consonant	mkhedruli		Შ	900		shini	U+10E8: GEORGIAN LETTER SHIN	š	sh	ş	✓
-Ს	S	s		S	s	V	Lu		consonant	mtavruli	ს		200		sani	U+1CA1: GEORGIAN MTAVRULI CAPITAL LETTER SAN	s	s	s	✓
-Შ	S	ʃ		Ʃ	sh	I	Lu		consonant	mtavruli	შ		900		shini	U+1CA8: GEORGIAN MTAVRULI CAPITAL LETTER SHIN	š	sh	ş	✓
-თ	t	tʰ		t	t	/	Ll		consonant	mkhedruli		Თ	9		tani	U+10D7: GEORGIAN LETTER TAN	t'	t'	t	✓
-ტ	t	tʼ		ṫ	t'	m	Ll		consonant	mkhedruli		Ტ	300		t'ari	U+10E2: GEORGIAN LETTER TAR	t	t	ť	✓
-ც	t	t͡sʰ		ʦ	ts	]	Ll		consonant	mkhedruli		Ც	2000		tsani	U+10EA: GEORGIAN LETTER CAN	c’	ts'	ts	✓
-წ	t	t͡sʼ		ʦ̇	ts'	o	Ll		consonant	mkhedruli		Წ	4000		ts'ili	U+10EC: GEORGIAN LETTER CIL	c	ts	ts’	✓
-Თ	T	tʰ		T	t	F	Lu		consonant	mtavruli	თ		9		tani	U+1C97: GEORGIAN MTAVRULI CAPITAL LETTER TAN	t'	t'	t	✓
-Ტ	T	tʼ		Ṫ	t'	M	Lu		consonant	mtavruli	ტ		300		t'ari	U+1CA2: GEORGIAN MTAVRULI CAPITAL LETTER TAR	t	t	ť	✓
-Ც	T	t͡sʰ		Tˢ	ts	}	Lu		consonant	mtavruli	ც		2000		tsani	U+1CAA: GEORGIAN MTAVRULI CAPITAL LETTER CAN	c’	ts'	ts	✓
-Წ	T	t͡sʼ		Ṫˢ	ts'	O	Lu		consonant	mtavruli	წ		4000		ts'ili	U+1CAC: GEORGIAN MTAVRULI CAPITAL LETTER CIL	c	ts	ts’	✓
-უ	u	u		u	u	e	Ll		vowel	mkhedruli		Უ	400		uni	U+10E3: GEORGIAN LETTER UN	u	u	u	✓
-Უ	U	u		U	u	E	Lu		vowel	mtavruli	უ		400		uni	U+1CA3: GEORGIAN MTAVRULI CAPITAL LETTER UN	u	u	u	✓
-ვ	v	v		v	v	d	Ll		consonant	mkhedruli		Ვ	6		vini	U+10D5: GEORGIAN LETTER VIN	v	v	v	✓
-Ვ	V	v		V	v	D	Lu		consonant	mtavruli	ვ		6		vini	U+1C95: GEORGIAN MTAVRULI CAPITAL LETTER VIN	v	v	v	✓
-ჳ	w	uɪ		w		d	Ll	r	consonant	mkhedruli		Ჳ	400		vie	U+10F3: GEORGIAN LETTER WE	w			✓
-Ჳ	W	uɪ		W		D	Lu	o	vowel	mtavruli	ჳ		400		vie	U+1CB3: GEORGIAN MTAVRULI CAPITAL LETTER WE	w			✓
-ხ	x	x χ		χ	kh	[	Ll		consonant	mkhedruli		Ხ	6000		khani	U+10EE: GEORGIAN LETTER XAN	x	kh	x	✓
-Ხ	X	x χ		Χ	kh	{	Lu		consonant	mtavruli	ხ		6000		khani	U+1CAE: GEORGIAN MTAVRULI CAPITAL LETTER XAN	x	kh	x	✓
-ღ	y	ɣ		ɣ	gh	q	Ll		consonant	mkhedruli		Ღ	700		ghani	U+10E6: GEORGIAN LETTER GHAN	ḡ	gh	ğ	✓
-Ღ	Y	ɣ		Ɣ	gh	Q	Lu		consonant	mtavruli	ღ		700		ghani	U+1CA6: GEORGIAN MTAVRULI CAPITAL LETTER GHAN	ḡ	gh	ğ	✓
-ზ	z	z		z	z	p	Ll		consonant	mkhedruli		Ზ	7		zeni	U+10D6: GEORGIAN LETTER ZEN	z	z	z	✓
-ჟ	z	ʒ		ʒ	zh		Ll		consonant	mkhedruli		Ჟ	90		zhani	U+10DF: GEORGIAN LETTER ZHAR	ž	zh	j	✓
-Ზ	Z	z		Z	z	P	Lu		consonant	mtavruli	ზ		7		zeni	U+1C96: GEORGIAN MTAVRULI CAPITAL LETTER ZEN	z	z	z	✓
-Ჟ	Z	ʒ		Ʒ	zh	"	Lu		consonant	mtavruli	ჟ		90		zhani	U+1C9F: GEORGIAN MTAVRULI CAPITAL LETTER ZHAR	ž	zh	j	✓
+																				
+ფ	p¶	pʰ		p	p	a	Ll		mkhedruli consonant			Ფ	500		pari	U+10E4: GEORGIAN LETTER PHAR	p'	p'	p	✓
+Ფ	P	pʰ		P	p	A	Lu		mtavruli consonant		ფ		500		pari	U+1CA4: GEORGIAN MTAVRULI CAPITAL LETTER PHAR	p'	p'	p	✓
+პ	p	pʼ		ṗ	p'	h	Ll		mkhedruli consonant			Პ	80		p'ari	U+10DE: GEORGIAN LETTER PAR	p	p	ṗ	✓
+Პ	P	pʼ		Ṗ	p'	H	Lu		mtavruli consonant		პ		80		p'ari	U+1C9E: GEORGIAN MTAVRULI CAPITAL LETTER PAR	p	p	ṗ	✓
+ბ	b	b		b	b	.	Ll		mkhedruli consonant			Ბ	2		bani	U+10D1: GEORGIAN LETTER BAN	b	b	b	✓
+Ბ	B	b		B	b	>	Lu		mtavruli consonant		ბ		2		bani	U+1C91: GEORGIAN MTAVRULI CAPITAL LETTER BAN	b	b	b	✓
+თ	t	tʰ		t	t	/	Ll		mkhedruli consonant			Თ	9		tani	U+10D7: GEORGIAN LETTER TAN	t'	t'	t	✓
+Თ	T	tʰ		T	t	F	Lu		mtavruli consonant		თ		9		tani	U+1C97: GEORGIAN MTAVRULI CAPITAL LETTER TAN	t'	t'	t	✓
+ტ	t	tʼ		ṫ	t'	m	Ll		mkhedruli consonant			Ტ	300		t'ari	U+10E2: GEORGIAN LETTER TAR	t	t	ť	✓
+Ტ	T	tʼ		Ṫ	t'	M	Lu		mtavruli consonant		ტ		300		t'ari	U+1CA2: GEORGIAN MTAVRULI CAPITAL LETTER TAR	t	t	ť	✓
+ც	t	t͡sʰ		ʦ	ts	]	Ll		mkhedruli consonant			Ც	2000		tsani	U+10EA: GEORGIAN LETTER CAN	c’	ts'	ts	✓
+Ც	T	t͡sʰ		Tˢ	ts	}	Lu		mtavruli consonant		ც		2000		tsani	U+1CAA: GEORGIAN MTAVRULI CAPITAL LETTER CAN	c’	ts'	ts	✓
+წ	t	t͡sʼ		ʦ̇	ts'	o	Ll		mkhedruli consonant			Წ	4000		ts'ili	U+10EC: GEORGIAN LETTER CIL	c	ts	ts’	✓
+Წ	T	t͡sʼ		Ṫˢ	ts'	O	Lu		mtavruli consonant		წ		4000		ts'ili	U+1CAC: GEORGIAN MTAVRULI CAPITAL LETTER CIL	c	ts	ts’	✓
+ჩ	c	t͡ʃʰ		ʧ	ch	x	Ll		mkhedruli consonant			Ჩ	1000		chini	U+10E9: GEORGIAN LETTER CHIN	č'	ch'	ç	✓
+Ჩ	C	t͡ʃʰ		Tᶴ	ch	X	Lu		mtavruli consonant		ჩ		1000		chini	U+1CA9: GEORGIAN MTAVRULI CAPITAL LETTER CHIN	č'	ch'	ç	✓
+ჭ	c	t͡ʃʼ		ʧ̇	ch'	z	Ll		mkhedruli consonant			Ჭ	5000		ch'ari	U+10ED: GEORGIAN LETTER CHAR	č	ch	ç̌	✓
+Ჭ	C	t͡ʃʼ		Ṫᶴ	ch'	Z	Lu		mtavruli consonant		ჭ		5000		ch'ari	U+1CAD: GEORGIAN MTAVRULI CAPITAL LETTER CHAR	č	ch	ç̌	✓
+დ	d	d		d	d	;	Ll		mkhedruli consonant			Დ	4		doni	U+10D3: GEORGIAN LETTER DON	d	d	d	✓
+Დ	D	d		D	d	:	Lu		mtavruli consonant		დ		4		doni	U+1C93: GEORGIAN MTAVRULI CAPITAL LETTER DON	d	d	d	✓
+ძ	d	d͡z		ʣ	dz	s	Ll		mkhedruli consonant			Ძ	3000		dzili	U+10EB: GEORGIAN LETTER JIL	j	dz	ž	✓
+Ძ	D	d͡z		ǲ	dz	S	Lu		mtavruli consonant		ძ		3000		dzili	U+1CAB: GEORGIAN MTAVRULI CAPITAL LETTER JIL	j	dz	ž	✓
+ჯ	j	d͡ʒ		ʤ	j	w	Ll		mkhedruli consonant			Ჯ	8000		jani	U+10EF: GEORGIAN LETTER JHAN	ǰ	j	c	✓
+Ჯ	J	d͡ʒ		Dᶾ	j	W	Lu		mtavruli consonant		ჯ		8000		jani	U+1CAF: GEORGIAN MTAVRULI CAPITAL LETTER JHAN	ǰ	j	c	✓
+ქ	k	kʰ		k	k	,	Ll		mkhedruli consonant			Ქ	600		kani	U+10E5: GEORGIAN LETTER KHAR	k'	k'	k	✓
+Ქ	K	kʰ		K	k	<	Lu		mtavruli consonant		ქ		600		kani	U+1CA5: GEORGIAN MTAVRULI CAPITAL LETTER KHAR	k'	k'	k	✓
+კ	k	kʼ		k̇	k'	r	Ll		mkhedruli consonant			Კ	20		k'ani	U+10D9: GEORGIAN LETTER KAN	k	k	ǩ	✓
+Კ	K	kʼ		K̇	k'		Lu		mtavruli consonant		კ		20		k'ani	U+1C99: GEORGIAN MTAVRULI CAPITAL LETTER KAN	k	k	ǩ	✓
+გ	g	ɡ		g	g	u	Ll		mkhedruli consonant			Გ	3		gani	U+10D2: GEORGIAN LETTER GAN	g	g	g	✓
+Გ	G	ɡ		G	g	U	Lu		mtavruli consonant		გ		3		gani	U+1C92: GEORGIAN MTAVRULI CAPITAL LETTER GAN	g	g	g	✓
+ჴ	q	qʰ		q		[	Ll	r	mkhedruli consonant			Ჴ	7000		qari, hari	U+10F4: GEORGIAN LETTER HAR	ẖ	q'		✓
+Ჴ	Q	qʰ		Q		{	Lu	o	mtavruli consonant		ჴ		7000		qari, hari	U+1CB4: GEORGIAN MTAVRULI CAPITAL LETTER HAR	ẖ	q'		✓
+ყ	q	qʼ		q̇	q'	c	Ll		mkhedruli consonant			Ყ	800		q'ari	U+10E7: GEORGIAN LETTER QAR	q	q	q	✓
+Ყ	Q	qʼ		Q̇	q'	C	Lu		mtavruli consonant		ყ		800		q'ari	U+1CA7: GEORGIAN MTAVRULI CAPITAL LETTER QAR	q	q	q	✓
+ვ	v	v		v	v	d	Ll		mkhedruli consonant			Ვ	6		vini	U+10D5: GEORGIAN LETTER VIN	v	v	v	✓
+Ვ	V	v		V	v	D	Lu		mtavruli consonant		ვ		6		vini	U+1C95: GEORGIAN MTAVRULI CAPITAL LETTER VIN	v	v	v	✓
+ს	s	s		s	s	v	Ll		mkhedruli consonant			Ს	200		sani	U+10E1: GEORGIAN LETTER SAN	s	s	s	✓
+Ს	S	s		S	s	V	Lu		mtavruli consonant		ს		200		sani	U+1CA1: GEORGIAN MTAVRULI CAPITAL LETTER SAN	s	s	s	✓
+ზ	z	z		z	z	p	Ll		mkhedruli consonant			Ზ	7		zeni	U+10D6: GEORGIAN LETTER ZEN	z	z	z	✓
+Ზ	Z	z		Z	z	P	Lu		mtavruli consonant		ზ		7		zeni	U+1C96: GEORGIAN MTAVRULI CAPITAL LETTER ZEN	z	z	z	✓
+შ	s	ʃ		ʃ	sh	i	Ll		mkhedruli consonant			Შ	900		shini	U+10E8: GEORGIAN LETTER SHIN	š	sh	ş	✓
+Შ	S	ʃ		Ʃ	sh	I	Lu		mtavruli consonant		შ		900		shini	U+1CA8: GEORGIAN MTAVRULI CAPITAL LETTER SHIN	š	sh	ş	✓
+ჟ	z	ʒ		ʒ	zh		Ll		mkhedruli consonant			Ჟ	90		zhani	U+10DF: GEORGIAN LETTER ZHAR	ž	zh	j	✓
+Ჟ	Z	ʒ		Ʒ	zh	"	Lu		mtavruli consonant		ჟ		90		zhani	U+1C9F: GEORGIAN MTAVRULI CAPITAL LETTER ZHAR	ž	zh	j	✓
+ხ	x	x χ		χ	kh	[	Ll		mkhedruli consonant			Ხ	6000		khani	U+10EE: GEORGIAN LETTER XAN	x	kh	x	✓
+Ხ	X	x χ		Χ	kh	{	Lu		mtavruli consonant		ხ		6000		khani	U+1CAE: GEORGIAN MTAVRULI CAPITAL LETTER XAN	x	kh	x	✓
+ღ	y	ɣ		ɣ	gh	q	Ll		mkhedruli consonant			Ღ	700		ghani	U+10E6: GEORGIAN LETTER GHAN	ḡ	gh	ğ	✓
+Ღ	Y	ɣ		Ɣ	gh	Q	Lu		mtavruli consonant		ღ		700		ghani	U+1CA6: GEORGIAN MTAVRULI CAPITAL LETTER GHAN	ḡ	gh	ğ	✓
+ჰ	h	h		h	h	?	Ll		mkhedruli consonant			Ჰ	9000		hae	U+10F0: GEORGIAN LETTER HAE	h	h	h	✓
+Ჰ	H	h		H	h	?	Lu		mtavruli consonant		ჰ		9000		hae	U+1CB0: GEORGIAN MTAVRULI CAPITAL LETTER HAE	h	h	h	✓
+მ	m	m		m	m	b	Ll		mkhedruli consonant			Მ	40		mani	U+10DB: GEORGIAN LETTER MAN	m	m	m	✓
+Მ	M	m		M	m	B	Lu		mtavruli consonant		მ		40		mani	U+1C9B: GEORGIAN MTAVRULI CAPITAL LETTER MAN	m	m	m	✓
+ნ	n	n		n	n	y	Ll		mkhedruli consonant			Ნ	50		nari	U+10DC: GEORGIAN LETTER NAR	n	n	n	✓
+Ნ	N	n		N	n	Y	Lu		mtavruli consonant		ნ		50		nari	U+1C9C: GEORGIAN MTAVRULI CAPITAL LETTER NAR	n	n	n	✓
+რ	r	r		r	r	j	Ll		mkhedruli consonant			Რ	100		rae	U+10E0: GEORGIAN LETTER RAE	r	r	r	✓
+Რ	R	r		R	r	J	Lu		mtavruli consonant		რ		100		rae	U+1CA0: GEORGIAN MTAVRULI CAPITAL LETTER RAE	r	r	r	✓
+ლ	l	l		l	l	l	Ll		mkhedruli consonant			Ლ	30		lasi	U+10DA: GEORGIAN LETTER LAS	l	l	l	✓
+Ლ	L	l		L	l	L	Lu		mtavruli consonant		ლ		30		lasi	U+1C9A: GEORGIAN MTAVRULI CAPITAL LETTER LAS	l	l	l	✓
+ჲ	j	je		j		n	Ll	r	mkhedruli letter			Ჲ	60		hie	U+10F2: GEORGIAN LETTER HIE	y	j	y	✓
+Ჲ	J	je		J		N	Lu	o	mtavruli letter		ჲ		60		hie	U+1CB2: GEORGIAN MTAVRULI CAPITAL LETTER HIE	y	j	y	✓
+																				
+ი	i	i		i	i	n	Ll		mkhedruli vowel			Ი	10		ini	U+10D8: GEORGIAN LETTER IN	i	i	i	✓
+Ი	I	i		I	i	N	Lu		mtavruli vowel		ი		10		ini	U+1C98: GEORGIAN MTAVRULI CAPITAL LETTER IN	i	i	i	✓
+უ	u	u		u	u	e	Ll		mkhedruli vowel			Უ	400		uni	U+10E3: GEORGIAN LETTER UN	u	u	u	✓
+Უ	U	u		U	u	E	Lu		mtavruli vowel		უ		400		uni	U+1CA3: GEORGIAN MTAVRULI CAPITAL LETTER UN	u	u	u	✓
+ჵ	o	oː		o		/	Ll	a	mkhedruli vowel			Ჵ	10000		hoe	U+10F5: GEORGIAN LETTER HOE	ō			✓
+Ჵ	O	oː		O		?	Lu	a	mtavruli vowel		ჵ		10000		hoe	U+1CB5: GEORGIAN MTAVRULI CAPITAL LETTER HOE	ō			✓
+ე	e	ɛ		ɛ	e	t	Ll		mkhedruli vowel			Ე	5		eni	U+10D4: GEORGIAN LETTER EN	e	e	e	✓
+Ე	E	ɛ		Ɛ	e	T	Lu		mtavruli vowel		ე		5		eni	U+1C94: GEORGIAN MTAVRULI CAPITAL LETTER EN	e	e	e	✓
+ო	o	ɔ		ɔ	o	k	Ll		mkhedruli vowel			Ო	70		oni	U+10DD: GEORGIAN LETTER ON	o	o	o	✓
+Ო	O	ɔ		Ɔ	o	K	Lu		mtavruli vowel		ო		70		oni	U+1C9D: GEORGIAN MTAVRULI CAPITAL LETTER ON	o	o	o	✓
+ა	a	ɑ		a	a	g	Ll		mkhedruli vowel			Ა	1		ani	U+10D0: GEORGIAN LETTER AN	a	a	a	✓
+Ა	A	ɑ		A	a	G	Lu		mtavruli vowel		ა		1		ani	U+1C90: GEORGIAN MTAVRULI CAPITAL LETTER AN	a	a	a	✓
+																				
+ჳ	w	uɪ		w		d	Ll	o	mkhedruli diphthong			Ჳ	400		vie	U+10F3: GEORGIAN LETTER WE	w			✓
+Ჳ	W	uɪ		W		D	Lu	o	mtavruli diphthong		ჳ		400		vie	U+1CB3: GEORGIAN MTAVRULI CAPITAL LETTER WE	w			✓
+ჱ	e	eɪ		e		y	Ll	o	mkhedruli diphthong			Ჱ	8		he	U+10F1: GEORGIAN LETTER HE	ē	ey		✓
+Ჱ	E	eɪ		E		T	Lu	o	mtavruli diphthong		ჱ		8		he	U+1CB1: GEORGIAN MTAVRULI CAPITAL LETTER HE	ē	ey		✓
 																				
 Ⴀ	A	ɑ		A̱	a	G	Lu	u	asomtravuli letter		ⴀ		1		ani	U+10A0: GEORGIAN CAPITAL LETTER AN	a	a	a	✓
 ⴀ	a	ɑ		a̱	a	g	Ll	u	nuskhuri letter			Ⴀ	1		ani	U+2D00: GEORGIAN SMALL LETTER AN	a	a	a	✓

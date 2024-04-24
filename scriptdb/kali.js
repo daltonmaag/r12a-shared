25515127d573267a1d29bd03f6f41db49442fffe
r12a <ishida@w3.org>
2024-04-24 15:39:28 +0100

    kali-kyu: Feb template update.

--- scriptdb/kali.js
+++ scriptdb/kali.js
@@ -18,10 +18,10 @@ var spreadsheet = `kali-kyu	key	ipa	ipa+	translit	transc	kbd	class	status	type	u
 ]	)			]	]		Pe	?	bracket				U+005D: RIGHT SQUARE BRACKET	
 %	%			%		%	Po	?	percentage mark				U+0025: PERCENT SIGN	✓
 														
-														
 ꤬	^	˨		¹	¹		Mn		low tone marker				U+A92C: KAYAH LI TONE CALYA	✓
 ꤭	^	˧		³	³		Mn		mid tone marker				U+A92D: KAYAH LI TONE CALYA PLOPHU	✓
 ꤫	^	˥		²	²		Mn		high tone marker				U+A92B: KAYAH LI TONE PLOPHU	✓
+														
 ꤁	±			1̣	1	1	Nd		digit				U+A901: KAYAH LI DIGIT ONE	✓
 ꤂	±			2̣	2	2	Nd		digit				U+A902: KAYAH LI DIGIT TWO	✓
 ꤃	±			3̣	3	3	Nd		digit				U+A903: KAYAH LI DIGIT THREE	✓
@@ -40,55 +40,54 @@ var spreadsheet = `kali-kyu	key	ipa	ipa+	translit	transc	kbd	class	status	type	u
 ”	>			”	”	&	Pf		quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	✓
 ’	>			\’	\’		Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
 ꤯	|			|	.	/	Po		phrase/sentence delimiter				U+A92F: KAYAH LI SIGN SHYA	✓
-ꤢ	a	a		+	a	f	Lo		vowel				U+A922: KAYAH LI LETTER A	✓
-ꤙ	b	b		b	b	e	Lo		stop				U+A919: KAYAH LI LETTER BA	✓
-ꤘ	d	d		d	d	[	Lo		stop				U+A918: KAYAH LI LETTER DA	✓
-ꤡ	d	d͡ʒ		ʤ	c	z	Lo		affricate				U+A921: KAYAH LI LETTER CA	✓
-ꤣ	e	ə		ə	ơ	t	Lo		vowel				U+A923: KAYAH LI LETTER OE	✓
-ꤩ	e	e		e	ê	g	Mn		vowel-sign				U+A929: KAYAH LI VOWEL EE	✓
-ꤧ	e	ɛ		ɛ	e		Mn		vowel-sign				U+A927: KAYAH LI VOWEL E	✓
-ꤌ	g	ɡ		ɡ	g	x	Lo	l	stop	used for loan words			U+A90C: KAYAH LI LETTER GA	✓
-ꤟ	h	h		h	h	k	Lo		fricative				U+A91F: KAYAH LI LETTER HA	✓
+														
+ꤕ	p	p		p	p	l	Lo		consonant				U+A915: KAYAH LI LETTER PA	✓
+ꤖ	P	Pʰ		pʰ	ph	n	Lo		aspirated consonant				U+A916: KAYAH LI LETTER PHA	✓
+ꤙ	b	b		b	b	e	Lo		consonant				U+A919: KAYAH LI LETTER BA	✓
+ꤒ	t	t		t	t	j	Lo		consonant				U+A912: KAYAH LI LETTER TA	✓
+ꤓ	T	Tʰ		tʰ	ht	o	Lo		aspirated consonant				U+A913: KAYAH LI LETTER HTA	✓
+ꤘ	d	d		d	d	[	Lo		consonant				U+A918: KAYAH LI LETTER DA	✓
+ꤡ	d	d͡ʒ		ʤ	c	z	Lo		consonant				U+A921: KAYAH LI LETTER CA	✓
+ꤊ	k	k		k	k	a	Lo		consonant				U+A90A: KAYAH LI LETTER KA	✓
+ꤋ	K	Kʰ		kʰ	kh	v	Lo		consonant				U+A90B: KAYAH LI LETTER KHA	✓
+ꤌ	g	ɡ		ɡ	g	x	Lo	l	consonant				U+A90C: KAYAH LI LETTER GA	✓
+ꤠ	v	v		v	v	q	Lo		consonant				U+A920: KAYAH LI LETTER VA	✓
+ꤎ	s	s		s	s	w	Lo	l	consonant				U+A90E: KAYAH LI LETTER SA	✓
+ꤏ	S	Sʰ		sʰ	sh	c	Lo		aspirated consonant				U+A90F: KAYAH LI LETTER SHA	✓
+ꤐ	z	z		z	zh	,	Lo		consonant				U+A910: KAYAH LI LETTER ZA	✓
+ꤞ	q	ʂ θ		ʂ	th	i	Lo		consonant				U+A91E: KAYAH LI LETTER THA	✓
+ꤟ	h	h		h	h	k	Lo		consonant				U+A91F: KAYAH LI LETTER HA	✓
+ꤗ	m	m		m	m	b	Lo		consonant				U+A917: KAYAH LI LETTER MA	✓
+ꤔ	n	n		n	n	s	Lo		consonant				U+A914: KAYAH LI LETTER NA	✓
+ꤑ	n	ɲ		ɲ	ny	p	Lo		consonant				U+A911: KAYAH LI LETTER NYA	✓
+ꤍ	N	ŋ		ŋ	ng		Lo		consonant				U+A90D: KAYAH LI LETTER NGA	✓
+ꤝ	w	w		w	w	m	Lo		consonant				U+A91D: KAYAH LI LETTER WA	✓
+ꤚ	r	r		r	r	r	Lo		consonant				U+A91A: KAYAH LI LETTER RA	✓
+ꤜ	l	l		l	l	;	Lo		consonant				U+A91C: KAYAH LI LETTER LA	✓
+ꤛ	j	j		j	j	d	Lo		consonant				U+A91B: KAYAH LI LETTER YA	✓
+														
 ꤤ	i	i		i	i	y	Lo		vowel				U+A924: KAYAH LI LETTER I	✓
-ꤛ	j	j		j	j	d	Lo		approximant				U+A91B: KAYAH LI LETTER YA	✓
-ꤊ	k	k		k	k	a	Lo		stop				U+A90A: KAYAH LI LETTER KA	✓
-ꤋ	K	Kʰ		kʰ	kh	v	Lo		stop				U+A90B: KAYAH LI LETTER KHA	✓
-ꤜ	l	l		l	l	;	Lo		lateral				U+A91C: KAYAH LI LETTER LA	✓
-ꤗ	m	m		m	m	b	Lo		nasal				U+A917: KAYAH LI LETTER MA	✓
-ꤔ	n	n		n	n	s	Lo		nasal				U+A914: KAYAH LI LETTER NA	✓
-ꤑ	n	ɲ		ɲ	ny	p	Lo		nasal				U+A911: KAYAH LI LETTER NYA	✓
-ꤍ	N	ŋ		ŋ	ng		Lo		nasal				U+A90D: KAYAH LI LETTER NGA	✓
-ꤥ	o	o		o	ô	u	Lo		vowel				U+A925: KAYAH LI LETTER OO	✓
-ꤪ	o	ɔ		ɔ	o	h	Mn		vowel-sign				U+A92A: KAYAH LI VOWEL O	✓
-ꤕ	p	p		p	p	l	Lo		stop				U+A915: KAYAH LI LETTER PA	✓
-ꤖ	P	Pʰ		pʰ	ph	n	Lo		aspirated stop				U+A916: KAYAH LI LETTER PHA	✓
-ꤞ	q	ʂ θ		ʂ	th	i	Lo		fricative				U+A91E: KAYAH LI LETTER THA	✓
-ꤚ	r	r		r	r	r	Lo		trill				U+A91A: KAYAH LI LETTER RA	✓
-ꤏ	S	Sʰ		sʰ	sh	c	Lo		aspirated fricative				U+A90F: KAYAH LI LETTER SHA	✓
-ꤎ	s	s		s	s	w	Lo	l	fricative	used for loan words			U+A90E: KAYAH LI LETTER SA	✓
-ꤒ	t	t		t	t	j	Lo		stop				U+A912: KAYAH LI LETTER TA	✓
-ꤓ	T	Tʰ		tʰ	ht	o	Lo		aspirated stop				U+A913: KAYAH LI LETTER HTA	✓
-ꤦ	U	ɯ		ɯ	ư		Mn		vowel-sign				U+A926: KAYAH LI VOWEL UE	✓
-ꤨ	u	u		u	u		Mn		vowel-sign				U+A928: KAYAH LI VOWEL U	✓
-ꤠ	v	v		v	v	q	Lo		fricative				U+A920: KAYAH LI LETTER VA	✓
-ꤝ	w	w		w	w	m	Lo		approximant				U+A91D: KAYAH LI LETTER WA	✓
-ꤐ	z	z		z	zh	,	Lo		fricative				U+A910: KAYAH LI LETTER ZA	✓
-														
-														
 ꤢꤦ		ɯ		aɯ	ư	F			vowel				U+A922 U+A926: KAYAH LI LETTER A, VOWEL UE	
+ꤦ	U	ɯ		ɯ	ư		Mn		vowel				U+A926: KAYAH LI VOWEL UE	✓
 ꤢꤨ		u		au	u	H			vowel				U+A922 U+A928: KAYAH LI LETTER A, VOWEL U	
+ꤨ	u	u		u	u		Mn		vowel				U+A928: KAYAH LI VOWEL U	✓
 ꤢꤩ		e		ae	ê	J			vowel				U+A922 U+A929: KAYAH LI LETTER A, VOWEL EE	
+ꤩ	e	e		e	ê	g	Mn		vowel				U+A929: KAYAH LI VOWEL EE	✓
+ꤥ	o	o		o	ô	u	Lo		vowel				U+A925: KAYAH LI LETTER OO	✓
+ꤣ	e	ə		ə	ơ	t	Lo		vowel				U+A923: KAYAH LI LETTER OE	✓
 ꤢꤧ		Ɛ		aɛ	e	G			vowel				U+A922 U+A927: KAYAH LI LETTER A, VOWEL E	
+ꤧ	e	ɛ		ɛ	e		Mn		vowel				U+A927: KAYAH LI VOWEL E	✓
 ꤢꤪ		ɔ		aɔ	o	K			vowel				U+A922 U+A92A: KAYAH LI LETTER A, VOWEL O	
+ꤪ	o	ɔ		ɔ	o	h	Mn		vowel				U+A92A: KAYAH LI VOWEL O	✓
+ꤢ	a	a		+	a	f	Lo		vowel				U+A922: KAYAH LI LETTER A	✓
 														
 ꤛꤣ		ɯə		jə	ươ				non-breathy diphthong				U+A91B U+A923: KAYAH LI LETTER YA, LETTER OE	
 ꤟꤌꤣ		ɯ̤ə		hgə	ươ				breathy diphthong				U+A91F U+A90C U+A923: KAYAH LI LETTER HA, LETTER GA, LETTER OE	
-														
-ꤣꤦ		əɯ			ơư			l	non-native vowel	loanwords			U+A923 U+A926: KAYAH LI LETTER OE, VOWEL UE	
-ꤣꤧ		ɯɛ			ưe			l	non-native vowel	loanwords			U+A923 U+A927: KAYAH LI LETTER OE, VOWEL E	
-ꤣꤨ		əu			ơu			l	non-native vowel	loanwords			U+A923 U+A928: KAYAH LI LETTER OE, VOWEL U	
-ꤣꤩ		əe			ơê			l	non-native vowel	loanwords			U+A923 U+A929: KAYAH LI LETTER OE, VOWEL EE	
-ꤣꤪ		əɔ			ơo			l	non-native vowel	loanwords			U+A923 U+A92A: KAYAH LI LETTER OE, VOWEL O	
+ꤣꤧ		ɯɛ			ưe			l	diphthong				U+A923 U+A927: KAYAH LI LETTER OE, VOWEL E	
+ꤣꤦ		əɯ			ơư			l	diphthong				U+A923 U+A926: KAYAH LI LETTER OE, VOWEL UE	
+ꤣꤨ		əu			ơu			l	diphthong				U+A923 U+A928: KAYAH LI LETTER OE, VOWEL U	
+ꤣꤩ		əe			ơê			l	diphthong				U+A923 U+A929: KAYAH LI LETTER OE, VOWEL EE	
+ꤣꤪ		əɔ			ơo			l	diphthong				U+A923 U+A92A: KAYAH LI LETTER OE, VOWEL O	
 														
 ʼ				ʼ	ʼ		Lm	?	apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE	
 §				§	§		Po	?	section sign				U+00A7: SECTION SIGN	

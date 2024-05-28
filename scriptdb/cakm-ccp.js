e861f0195e29ef6d5c634453bc5d5f46ee5603e5
r12a <ishida@w3.org>
2024-05-28 10:11:55 +0100

    Chakma db and webfont

---
+++ scriptdb/cakm-ccp.js
@@ -0,0 +1,209 @@
+var spreadsheet = `cakm-cpp	key	ipa	ipa+	translit	transc	kbd	class	status	type	usage	native name	name transcription	ucs name	block
+𑄛	p	p	aː	p	p		Lo		consonant				U+1111B: CHAKMA LETTER PAA	✓
+𑄜	p	pʰ	aː	pʰ	ph		Lo		consonant				U+1111C: CHAKMA LETTER PHAA	✓
+𑄝	b	b	aː	b	b		Lo		consonant				U+1111D: CHAKMA LETTER BAA	✓
+𑄞	b	bʰ	aː	bʰ	bh		Lo		consonant				U+1111E: CHAKMA LETTER BHAA	✓
+𑄖	t	t	aː	t	t		Lo		consonant				U+11116: CHAKMA LETTER TAA	✓
+𑄗	t	tʰ	aː	tʰ	th		Lo		consonant				U+11117: CHAKMA LETTER THAA	✓
+𑄌	c	t͡ʃ	aː	ʧ	c		Lo		consonant				U+1110C: CHAKMA LETTER CAA	✓
+𑄍	c	t͡ʃʰ	aː	ʧʰ	ch		Lo		consonant				U+1110D: CHAKMA LETTER CHAA	✓
+𑄘	d	d	aː	d	d		Lo		consonant				U+11118: CHAKMA LETTER DAA	✓
+𑄙	d	dʰ	aː	dʰ	dh		Lo		consonant				U+11119: CHAKMA LETTER DHAA	✓
+𑄎	j	d͡ʒ	aː	ʤ	j		Lo		consonant				U+1110E: CHAKMA LETTER JAA	✓
+𑄏	j	d͡ʒʰ	aː	ʤʰ	jh		Lo		consonant				U+1110F: CHAKMA LETTER JHAA	✓
+𑄑	t	ʈ	aː	ʈ	ṭ		Lo		consonant				U+11111: CHAKMA LETTER TTAA	✓
+𑄒	t	ʈʰ	aː	ʈʰ	ṭh		Lo		consonant				U+11112: CHAKMA LETTER TTHAA	✓
+𑄓	d	ɖ	aː	ɖ	ḍ		Lo		consonant				U+11113: CHAKMA LETTER DDAA	✓
+𑄔	d	ɖʰ	aː	ɖʰ	ḍh		Lo		consonant				U+11114: CHAKMA LETTER DDHAA	✓
+𑄇	k	k	aː	k	k		Lo		consonant				U+11107: CHAKMA LETTER KAA	✓
+𑄈	k	kʰ	aː	kʰ	kh		Lo		consonant				U+11108: CHAKMA LETTER KHAA	✓
+𑄉	g	ɡ	aː	ɡ	g		Lo		consonant				U+11109: CHAKMA LETTER GAA	✓
+𑄊	g	ɡʰ	aː	ɡʰ	gh		Lo		consonant				U+1110A: CHAKMA LETTER GHAA	✓
+𑅇	v	v	aː	v	v		Lo	l	consonant	Used for Pali.			U+11147: CHAKMA LETTER VAA	✓
+𑄥	s	s ʃ	aː	s	s		Lo		consonant				U+11125: CHAKMA LETTER SAA	✓
+𑄡	y	z	aː	z	z		Lo		consonant				U+11121: CHAKMA LETTER YAA	✓
+𑄦	h	h	aː	h	h		Lo		consonant				U+11126: CHAKMA LETTER HAA	✓
+𑄟	m	m	aː	m	m		Lo		consonant				U+1111F: CHAKMA LETTER MAA	✓
+𑄚	n	n	aː	n	n		Lo		consonant				U+1111A: CHAKMA LETTER NAA	✓
+𑄕	n	ɳ	aː	ɳ	ṇ		Lo		consonant				U+11115: CHAKMA LETTER NNAA	✓
+𑄐	n	ɲ	aː	ɲ	ñ		Lo		consonant				U+11110: CHAKMA LETTER NYAA	✓
+𑄋	n	ŋ	aː	ŋ	ṅ		Lo		consonant				U+1110B: CHAKMA LETTER NGAA	✓
+𑄤	w	w	aː	w	w		Lo		consonant				U+11124: CHAKMA LETTER WAA	✓
+𑄢	r	r ɽ	aː	r	r		Lo		consonant				U+11122: CHAKMA LETTER RAA	✓
+𑄣	l	l	aː	l	l		Lo		consonant				U+11123: CHAKMA LETTER LAA	✓
+𑅄	l	lʰ	aː	lʰ	ḷh		Lo	r	consonant	Used by the Baarah Maatraa orthography.			U+11144: CHAKMA LETTER LHAA	✓
+𑄠	y	j	aː	j	y		Lo		consonant				U+11120: CHAKMA LETTER YYAA	✓
+														
+𑄀	[¶	̃		˜	̃		Mn		nasalisation marker				U+11100: CHAKMA SIGN CANDRABINDU	✓
+𑄁	n	ŋ		ŋ̽	ṃ		Mn		final nasal				U+11101: CHAKMA SIGN ANUSVARA	✓
+𑄂	h	h		h̽	h		Mn		final aspiration				U+11102: CHAKMA SIGN VISARGA	✓
+														
+𑄨	i	i		i	i		Mn		vowel sign				U+11128: CHAKMA VOWEL SIGN I	✓
+𑄄	i	i		ị	i		Lo	i	independent vowel				U+11104: CHAKMA LETTER I	✓
+𑄩	i	iː		iː	ī		Mn	i	vowel sign				U+11129: CHAKMA VOWEL SIGN II	✓
+𑄪	u	u		u	u		Mn		vowel sign				U+1112A: CHAKMA VOWEL SIGN U	✓
+𑄅	u	u		ụ	u		Lo	i	independent vowel				U+11105: CHAKMA LETTER U	✓
+𑄫	u	uː		uː	ū		Mn	i	vowel sign				U+1112B: CHAKMA VOWEL SIGN UU	✓
+𑄬	e	e ɛ		e	e		Mc		vowel sign				U+1112C: CHAKMA VOWEL SIGN E	✓
+𑄆	e	e		ẹ	e		Lo	i	independent vowel				U+11106: CHAKMA LETTER E	✓
+𑄮	o	o		o	o		Mn		vowel sign				U+1112E: CHAKMA VOWEL SIGN O	✓
+𑄧	a	ɔ		ɔ	o		Mn		vowel sign				U+11127: CHAKMA VOWEL SIGN A	✓
+𑄃	a	aː		aː	ā		Lo		independent vowel				U+11103: CHAKMA LETTER AA	✓
+𑅅	a	aː		a	ā		Mc	r	vowel sign	Used by the Baarah Maatraa orthography.			U+11145: CHAKMA VOWEL SIGN AA	✓
+														
+𑄪𑄃𑄨	u	ui̯		uaːi	ui				diphthong				U+1112A U+11103 U+11128: CHAKMA VOWEL SIGN U, LETTER AA, VOWEL SIGN I	
+𑅆		eːi̯		eⁱ	ei		Mc	r	diphthong	Used by the Baarah Maatraa orthography.			U+11146: CHAKMA VOWEL SIGN EI	
+𑄰	o	oi̯		oⁱ	oi		Mn	i	diphthong				U+11130: CHAKMA VOWEL SIGN OI	
+𑄯	a	ou̯		oᵘ	au		Mn		diphthong				U+1112F: CHAKMA VOWEL SIGN AU	
+𑄬𑄭	a	æ		eaⁱ	ae				diphthong				U+1112C U+1112D: CHAKMA VOWEL SIGN E, VOWEL SIGN AI	
+𑄃𑄨	a	ai̯		aːi	āi				diphthong				U+11103 U+11128: CHAKMA LETTER AA, VOWEL SIGN I	
+𑄭	a	ai̯		aⁱ	āi		Mn	i	diphthong				U+1112D: CHAKMA VOWEL SIGN AI	
+														
+𑄱							Mn		decomposed orphan				U+11131: CHAKMA O MARK	
+𑄲							Mn		decomposed orphan				U+11132: CHAKMA AU MARK	
+														
+𑄶	±	0		0̣	0		Nd		digit				U+11136: CHAKMA DIGIT ZERO	✓
+𑄷	±	1		1̣	1		Nd		digit				U+11137: CHAKMA DIGIT ONE	✓
+𑄸	±	2		2̣	2		Nd		digit				U+11138: CHAKMA DIGIT TWO	✓
+𑄹	±	3		3̣	3		Nd		digit				U+11139: CHAKMA DIGIT THREE	✓
+𑄺	±	4		4̣	4		Nd		digit				U+1113A: CHAKMA DIGIT FOUR	✓
+𑄻	±	5		5̣	5		Nd		digit				U+1113B: CHAKMA DIGIT FIVE	✓
+𑄼	±	6		6̣	6		Nd		digit				U+1113C: CHAKMA DIGIT SIX	✓
+𑄽	±	7		7̣	7		Nd		digit				U+1113D: CHAKMA DIGIT SEVEN	✓
+𑄾	±	8		8̣	8		Nd		digit				U+1113E: CHAKMA DIGIT EIGHT	✓
+𑄿	±	9		9̣	9		Nd		digit				U+1113F: CHAKMA DIGIT NINE	✓
+০		0		0̣	0		Nd		digit				U+09E6: BENGALI DIGIT ZERO	
+১		1		1̣	1		Nd		digit				U+09E7: BENGALI DIGIT ONE	
+২		2		2̣	2		Nd		digit				U+09E8: BENGALI DIGIT TWO	
+৩		3		3̣	3		Nd		digit				U+09E9: BENGALI DIGIT THREE	
+৪		4		4̣	4		Nd		digit				U+09EA: BENGALI DIGIT FOUR	
+৫		5		5̣	5		Nd		digit				U+09EB: BENGALI DIGIT FIVE	
+৬		6		6̣	6		Nd		digit				U+09EC: BENGALI DIGIT SIX	
+৭		7		7̣	7		Nd		digit				U+09ED: BENGALI DIGIT SEVEN	
+৮		8		8̣	8		Nd		digit				U+09EE: BENGALI DIGIT EIGHT	
+৯		9		9̣	9		Nd		digit				U+09EF: BENGALI DIGIT NINE	
+														
+														
+// space														
+\u0020	 						Zs		space				U+0020: SPACE	
+\u202F	]			ⁿⁿᵇˢᵖ			Zs	?	nnbsp				U+202F NARROW NO-BREAK SPACE	
+// phrase dividers														
+,	,			,	,		Po		comma				U+002C: COMMA	
+;	;			;	;	;;⁏⸵	Po		semicolon				U+003B: SEMICOLON	
+:	:			: :̣ :̱ :͈ :͇	:		Po	?	colon				U+003A: COLON	
+!	!			! !̣ !̱ !͈ !͇	!		Po	?	exclamation mark				U+0021: EXCLAMATION MARK	
+𑅃	?			?̣	?		Po		question mark				U+11143: CHAKMA QUESTION MARK	
+?	?			? ?̣ ?̱ ?͈ ?͇	?		Po	?	question mark				U+003F: QUESTION MARK	
+// period & ellipsis														
+.	.			. .̣ .̱ .͈ .͇	.		Po	?	full stop				U+002E: FULL STOP	
+…	.			…	…		Po	?	ellipsis				U+2026: HORIZONTAL ELLIPSIS	
+⋯	.			⋯̇ ⋯̄ ⋯̎ ⋯̿	⋯		Sm	?	midline ellipsis				U+22EF: MIDLINE HORIZONTAL ELLIPSIS	
+// section dividers														
+𑅁	|			|	,		Po		phrase terminator				U+11141: CHAKMA DANDA	
+𑅂	|			|˙	.		Po		sentence terminator				U+11142: CHAKMA DOUBLE DANDA	
+𑅀	|			¶			Po		section divider				U+11140: CHAKMA SECTION MARK	
+// quote marks														
+«	<			«	“		Pi	?	quotation mark				U+00AB: LEFT-POINTING DOUBLE ANGLE QUOTATION MARK	
+»	>			»	”		Pf	?	quotation mark				U+00BB: RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK	
+‹	<			‹			Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+›	>			›			Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+“	<			“	“		Pi	?	quotation mark				U+201C: LEFT DOUBLE QUOTATION MARK	
+”	>			”	”		Pf	?	quotation mark				U+201D: RIGHT DOUBLE QUOTATION MARK	
+‘	<			‘	‘		Pi	?	quotation mark				U+2018: LEFT SINGLE QUOTATION MARK	
+’	>			’	’		Pf	?	quotation mark				U+2019: RIGHT SINGLE QUOTATION MARK	
+// brackets														
+(	(			(	(		Ps		parenthesis				U+0028: LEFT PARENTHESIS	
+)	)			)	)		Ps		parenthesis				U+0029: RIGHT PARENTHESIS	
+[	(			[	[		Pe	?	bracket				U+005B: LEFT SQUARE BRACKET	
+]	)			] 	]		Pe	?	bracket				U+005D: RIGHT SQUARE BRACKET	
+// dashes														
+-	-			-	-		Pd		hyphen				U+2010: HYPHEN	
+‑	-			‑	‑		Pd	?	non-breaking hyphen				U+2011: NON-BREAKING HYPHEN	
+–	-			–	–		Pd	?	en dash				U+2013: EN DASH	
+—	-			—	—		Pd	?	em dash				U+2014: EM DASH	
+// length marks														
+	:			ː ː̣ ː̱ ː͈ ː͇					length mark					
+// apostrophe														
+'	\'			ʼ	ʼ		Lm	?	apostrophe				U+02BC: MODIFIER LETTER APOSTROPHE	
+// virama, sukun, silencers, etc														
+𑄴	*			˟			Mn​		vowel absence indicator				U+11134: CHAKMA MAAYYAA	
+𑄳	*			͞			Mn​		vowel-killer, virama				U+11133: CHAKMA VIRAMA	
+// secondary viramas														
+	\			ˣ ˣ̣ ˣ̱ ˣ͈ ˣ͇				u						
+// nuktas														
+‍଼	/			ˑ ˑ̣ ˑ̱ ˑ͈ ˑ͇				u	nukta					
+// repetition, gemination														
+	&			& &̣ &̱ &͈ &͇ &̥				u	repetition					
+	&			˖				u	gemination					
+// abbrev.														
+	+			¨				u	abbreviation marker					
+// footnote markers														
+*	*			*	*		Po	u	asterisk				U+002A ASTERISK	
+§	*			§	§		Po	u	section sign				U+00A7: SECTION SIGN	
+†	*			†	†		Po	u	dagger				U+2020: DAGGER	
+‡	*			‡	‡		Po	u	double dagger				U+2021: DOUBLE DAGGER	
+// other														
+′	\'			′	′		Po	u	prime				U+2032: PRIME	
+″	"			″	″		Po	u	double prime				U+2033: DOUBLE PRIME	
+•	•			•			Po	u	bullet				U+2022: BULLET	
+// formatting characters														
+\u200C	=			ᶻʷⁿʲ			Cf		zero-width non-joiner				U+200C: ZERO WIDTH NON-JOINER	
+\u200D	=			ᶻʷʲ			Cf		zero-width joiner				U+200D: ZERO WIDTH JOINER	
+\u034F	=			ᶜᵍʲ			Mn		combining grapheme joiner				U+034F: COMBINING GRAPHEME JOINER	
+\u2060	=			ʷʲ			Cf		word joiner				U+2060 WORD JOINER	
+// number related														
+	#			ᵗ				u	thousands separator					
+	#			ᵈ				u	decimal separator					
+	#			/				u	date separator					
+	#			¤				u	currency symbol					
+	#			¢				u						
+	#			# #̣ #̱ #͈ #͇				u	number symbol					
+%	%			%			Po	u	percentage mark				U+0025: PERCENT SIGN	
+‰	%			‰			Po	u	per mille mark				U+2030: PER MILLE SIGN	
+// ascii digits														
+0	0			0	0		Nd	?	digit				U+0030: DIGIT ZERO	
+1	1			1	1		Nd	?	digit				U+0031: DIGIT ONE	
+2	2			2	2		Nd	?	digit				U+0032: DIGIT TWO	
+3	3			3	3		Nd	?	digit				U+0033: DIGIT THREE	
+4	4			4	4		Nd	?	digit				U+0034: DIGIT FOUR	
+5	5			5	5		Nd	?	digit				U+0035: DIGIT FIVE	
+6	6			6	6		Nd	?	digit				U+0036: DIGIT SIX	
+7	7			7	7		Nd	?	digit				U+0037: DIGIT SEVEN	
+8	8			8	8		Nd	?	digit				U+0038: DIGIT EIGHT	
+9	9			9	9		Nd	?	digit				U+0039: DIGIT NINE	
+`														
+														
+latinPanel = 'æ ñ ā ī ŋ ū ɔ ɖ ɛ ɡ ɲ ɳ ɽ ʃ ʈ ʒ ʰ ː ̃ i̯ u̯ t͡ʃ t͡ʃʰ d͡ʒ d͡ʒʰ ḍ ḷ ṃ ṅ ṇ ṭ'														
+														
+var cols = {														
+key: 1,														
+ipaLoc: 2,														
+ipaPlus: 3,														
+transLoc: 4,														
+transcription: 5,														
+kbd: 6,														
+class: 7,														
+status: 8,														
+typeLoc: 9,														
+statusLoc: 10,														
+														
+dvowel: 0,														
+ivowel: 0,														
+onset:0,														
+final:0,														
+subj:0,														
+ltone:0,														
+htone:0,														
+lc:0,														
+uc:0,														
+meaning:0,														
+shape: 0,														
+numLoc: 0,														
+														
+nnameLoc: 11,														
+nameLoc: 12,														
+ucsName: 13,														
+block: 14,														
+														
+//othertranscriptions: [[5, 'Latin']]														
+}														
